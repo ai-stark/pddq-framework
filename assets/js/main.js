0d2e83634ee571d5f3eb603f548fbb3d0c3d32a9
Scott Starkey <sstarkey@ainq.com>
2017-01-26 15:26:52 -0500

    worksheet layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,24 @@
 
+
+if(localStorage){
+	$('#notes').val(localStorage.getItem("notes"));
+	$('#background-1-1-attainment').val(localStorage.getItem("attainment"));
+}
+
+
+$('.worksheet-nav').on("click", function(event){
+	if(!$(this).hasClass("selected")){
+		$('.worksheet-nav').removeClass("selected");
+		$(this).addClass("selected");
+	}
+});
+
+
+
+
+$('#button').on("click", function(){
+	localStorage.clear();
+	localStorage.setItem("notes", $('#notes').val());
+	localStorage.setItem("attainment", $('#background-1-1-attainment').val());
+});
+
